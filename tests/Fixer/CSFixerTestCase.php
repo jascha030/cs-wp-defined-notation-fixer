b7f75f09f13ff2845efe94201cca238f1125a77e
Jascha030 <contact@jaschavanaalst.nl>
2021-12-24 19:09:26 +0100

    🔧 Composer init

---
+++ tests/Fixer/CSFixerTestCase.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Jascha030\PhpCsFixer\WordPress\DefinedNotation\Test\Fixer;
+
+class CSFixerTestAbstract
+{
+
+}
---
+++ tests/Fixer/WordPressABSPATHCheckNotationFixerTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Jascha030\PhpCsFixer\WordPress\DefinedNotation\Test\Fixer;
+
+use Jascha030\PhpCsFixer\WordPress\DefinedNotation\Fixer\WordPressABSPATHCheckNotationFixer;
+use PHPUnit\Framework\TestCase;
+
+class WordPressABSPATHCheckNotationFixerTest extends TestCase
+{
+
+}
